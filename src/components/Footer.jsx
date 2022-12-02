f0490ba755473c2294a2a9e2b968e38f50828fb1
Akhila <burraakhila1999@gmail.com>
2022-12-02 11:02:13 +0530

    Transaction List changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 function Footer() {
     return (
         <div className='footer'>
-            <footer class='py-4 bg-dark fixed-bottom'>
+           <footer class='py-4 bg-dark fixed-bottom'>
                 <div class='containerfooter' style={{height: 0}}>
                     <p class='m-0 text-center text-white'>
                         Copyright &copy; nt$ 2022
                     </p>
-                </div>
+                </div> 
             </footer>
         </div>
     );
--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -7,6 +7,9 @@ import {
   CDBSidebarMenu,
   CDBSidebarMenuItem,
 } from "cdbreact";
+import { Ripple } from "primereact/ripple";
+import { Dropdown } from "primereact/dropdown";
+import { classNames } from "primereact/utils";
 import { FilterMatchMode } from "primereact/api";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
@@ -29,6 +32,28 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+const moneyStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+const tradeStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 900,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const TransactionList = () => {
   const [transactions, setTransactions] = useState([]);
   const [moneyInfo, setMoneyInfo] = useState([]);
@@ -37,7 +62,14 @@ const TransactionList = () => {
   const [moneyOpen, setMoneyOpen] = useState(false);
   const [globalFilterValue1, setGlobalFilterValue1] = useState("");
   const [filters1, setFilters1] = useState(null);
+  const [first1, setFirst1] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rows1, setRows1] = useState(5);
   const [id, setId] = useState("");
+  const [pageInputTooltip, setPageInputTooltip] = useState(
+    "Press 'Enter' key to go to this page."
+  );
+
   var test;
   const handleClose = () => setTradeOpen(false);
   const handleMoneyClose = () => setMoneyOpen(false);
@@ -78,7 +110,121 @@ const TransactionList = () => {
       </div>
     );
   };
+  const onCustomPage1 = (event) => {
+    setFirst1(event.first);
+    setRows1(event.rows);
+    setCurrentPage(event.page + 1);
+  };
+  const template1 = {
+    layout:
+      "PrevPageLink PageLinks NextPageLink RowsPerPageDropdown CurrentPageReport",
+    PrevPageLink: (options) => {
+      return (
+        <button
+          type="button"
+          className={options.className}
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className="p-3">Previous</span>
+          <Ripple />
+        </button>
+      );
+    },
+    NextPageLink: (options) => {
+      return (
+        <button
+          type="button"
+          className={options.className}
+          onClick={options.onClick}
+          disabled={options.disabled}
+        >
+          <span className="p-3">Next</span>
+          <Ripple />
+        </button>
+      );
+    },
+    PageLinks: (options) => {
+      if (
+        (options.view.startPage === options.page &&
+          options.view.startPage !== 0) ||
+        (options.view.endPage === options.page &&
+          options.page + 1 !== options.totalPages)
+      ) {
+        const className = classNames(options.className, { "p-disabled": true });
 
+        return (
+          <span className={className} style={{ userSelect: "none" }}>
+            ...
+          </span>
+        );
+      }
+
+      return (
+        <button
+          type="button"
+          className={options.className}
+          onClick={options.onClick}
+        >
+          {options.page + 1}
+          <Ripple />
+        </button>
+      );
+    },
+    RowsPerPageDropdown: (options) => {
+      const dropdownOptions = [
+        { label: 5, value: 5 },
+        { label: 10, value: 10 },
+        { label: 15, value: 15 },
+        { label: "All", value: options.totalRecords },
+      ];
+
+      return (
+        <Dropdown
+          value={options.value}
+          options={dropdownOptions}
+          onChange={options.onChange}
+        />
+      );
+    },
+    CurrentPageReport: (options) => {
+      return (
+        <span
+          className="mx-3"
+          style={{ color: "var(--text-color)", userSelect: "none" }}
+        >
+          Go to{" "}
+          <InputText
+            size="2"
+            className="ml-1"
+            value={currentPage}
+            tooltip={pageInputTooltip}
+            onKeyDown={(e) => onPageInputKeyDown(e, options)}
+            onChange={onPageInputChange}
+          />
+        </span>
+      );
+    },
+  };
+  const onPageInputKeyDown = (event, options) => {
+    if (event.key === "Enter") {
+      const page = parseInt(currentPage);
+      if (page < 1 || page > options.totalPages) {
+        setPageInputTooltip(
+          `Value must be between 1 and ${options.totalPages}.`
+        );
+      } else {
+        const first = currentPage ? options.rows * (page - 1) : 0;
+
+        setFirst1(first);
+        setPageInputTooltip("Press 'Enter' key to go to this page.");
+      }
+    }
+  };
+
+  const onPageInputChange = (event) => {
+    setCurrentPage(event.target.value);
+  };
   const initFilters1 = () => {
     setFilters1({
       global: { value: null, matchMode: FilterMatchMode.CONTAINS },
@@ -141,7 +287,7 @@ const TransactionList = () => {
 
   return (
     <div
-      style={{ display: "flex", height: "80vh", overflow: "scroll initial" }}
+      style={{ display: "flex", height: "100vh", overflow: "scroll initial" }}
     >
       <CDBSidebar textColor="#fff" backgroundColor="#333">
         <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
@@ -200,6 +346,11 @@ const TransactionList = () => {
           "transactionTime",
           "transactionId",
         ]}
+        paginator
+        paginatorTemplate={template1}
+        first={first1}
+        rows={rows1}
+        onPage={onCustomPage1}
       >
         <Column field="transactionId" header="Transaction Id" sortable></Column>
         <Column
@@ -232,7 +383,7 @@ const TransactionList = () => {
         aria-describedby="modal-modal-description"
       >
         {tradeOpen && tradeInfo.length !== 0 ? (
-          <Box sx={style}>
+          <Box sx={tradeStyle}>
             <DataTable value={tradeInfo} responsiveLayout="scroll">
               <Column
                 field="tradeTransactionType"
@@ -275,7 +426,7 @@ const TransactionList = () => {
         aria-describedby="modal-modal-description"
       >
         {moneyOpen ? (
-          <Box sx={style}>
+          <Box sx={moneyStyle}>
             <table>
               <tr>
                 <th>Name</th>
